8b3209a92626ac1748dc1274248677062a52f0e2
ToffeeNutCroffle <cloyster539@gmail.com>
2025-02-14 20:16:26 +0900

    편의성패치
    -노트를 작성하기 위해 편의성 패치를 진행했습니다.

--- Assets/Scripts/StageScene/BeatsFolder/Sept.Beat.cs
+++ Assets/Scripts/StageScene/BeatsFolder/Sept.Beat.cs
@@ -8,37 +8,28 @@ public class SeptBeat : MonoBehaviour
     {
         //이 안에 비트 작성
         //StartCorutine(스폰할노트(시간(int면 숫자만 float면 뒤에 f)));
-        StartCoroutine(RightSpawn(0));
-        StartCoroutine(DownSpawn(0.9f));
-        StartCoroutine(RightSpawn(1.6f)); 
-        StartCoroutine(RightSpawn(1.9f));
-        StartCoroutine(DownSpawn(2.3f));
-        StartCoroutine(LeftSpawn(3));
-        StartCoroutine(LeftSpawn(3.4f));
-        StartCoroutine(UpSpawn(3.7f));
-        StartCoroutine(RightSpawn(4.4f));
-        StartCoroutine(DownSpawn(5.2f));
+        StartCoroutine(RightArrow(0));
 
     }
-    public IEnumerator LeftSpawn(float time)
+    public IEnumerator LeftArrow(float time)
     {
         yield return new WaitForSeconds(time);
         GameObject LeftArrow = BeatController.instance.PoolLeft.Get();
     }
 
-    public IEnumerator RightSpawn(float time)
+    public IEnumerator RightArrow(float time)
     {
         yield return new WaitForSeconds(time);
         GameObject RightArrow = BeatController.instance.PoolRight.Get();
     }
 
-    public IEnumerator UpSpawn(float time)
+    public IEnumerator UpArrow(float time)
     {
         yield return new WaitForSeconds(time);
         GameObject UptArrow = BeatController.instance.PoolUp.Get();
     }
 
-    public IEnumerator DownSpawn(float time)
+    public IEnumerator DownArrow(float time)
     {
         yield return new WaitForSeconds(time);
         GameObject DownArrow = BeatController.instance.PoolDown.Get();
--- Assets/Scripts/StageScene/ButtonController.cs
+++ Assets/Scripts/StageScene/ButtonController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class ButtonController : MonoBehaviour
 {
@@ -10,6 +12,30 @@ public class ButtonController : MonoBehaviour
 
     public KeyCode keyToPressR;
     public KeyCode keyToPressL; 
+
+    private static string logFilePath;
+    void Awake()
+    {
+        string directoryPath = @"C:\Users\Home\Documents\GitHub\rhythm_game_test\Assets\Scripts\StageScene\BeatsFolder";
+        logFilePath = Path.Combine(directoryPath, "log.txt");
+        try
+        {
+            // 디렉토리 존재 여부 확인 후 생성
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(logFilePath, "");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("로그 파일 초기화 실패: " + e.Message);
+        }
+    }
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +51,21 @@ public class ButtonController : MonoBehaviour
             if(Input.GetKeyDown(keyToPressR) || Input.GetKeyDown(keyToPressL))
             {
                 theSR.sprite = preesedImage;
-                Debug.Log($"StartCoroutime({keyToPressL}(" + string.Format("{0:N2}",GameManager.instance.systemtime-3.6f) + "f);");
+                WriteLog();
             }
 
             if(Input.GetKeyUp(keyToPressL) || Input.GetKeyUp(keyToPressR))
             {
-                theSR.sprite= defaultImage;
+                theSR.sprite = defaultImage;
             }
         }
+    }
+
+    public void WriteLog()
+    {
+        StreamWriter sw = new StreamWriter(logFilePath,true);
+        sw.WriteLine($"        StartCoroutine({keyToPressL}(" + string.Format("{0:N2}",GameManager.instance.systemtime-3.6f) + "f));");
+        sw.Flush();
+        sw.Close();
     }
 }
